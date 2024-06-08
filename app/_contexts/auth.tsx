578fbe29a3c3549fab61ce0aaf2c09f0e327c05f
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-06-08 18:10:26 -0300

    feat: add signUp function

--- app/_contexts/auth.tsx
+++ app/_contexts/auth.tsx
@@ -1,20 +1,60 @@
 import { PropsWithChildren, createContext, useState } from "react";
+import { useNavigation } from "expo-router";
+import api from "../_services/api";
+
+interface SignUpUserData {
+  signUpUser: { name: string; email: string; password: string };
+}
+
+interface UserData {
+  id: string;
+  name: string;
+  email: string;
+  balance: number;
+  created_at: string;
+  updated_at: string;
+}
 
 interface AuthContextData {
-  user: { name: string; email: string };
-  setUser: (user: { name: string; email: string }) => void;
+  user: UserData | null;
+  loading: boolean;
+  setUser: (user: UserData) => void;
+  signUp: (signUpUser: SignUpUserData) => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextData);
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
-  const [user, setUser] = useState({
-    name: "John Doe",
-    email: "john@doe.com",
-  });
+  const [user, setUser] = useState<UserData | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigation();
+
+  const signUp = async ({ signUpUser }: SignUpUserData) => {
+    if (
+      signUpUser.name === "" ||
+      signUpUser.email === "" ||
+      signUpUser.password === ""
+    ) {
+      return alert("Preencha todos os campos!");
+    }
+
+    setLoading(true);
+    try {
+      const response = await api.post("/users", signUpUser);
+
+      setUser(response.data);
+      navigate.navigate("(login)/index" as never);
+      console.log("Usuário cadastrado com sucesso!", response.data);
+    } catch (error) {
+      console.log("Erro ao tentar cadastrar usuário.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, loading, setUser, signUp }}>
       {children}
     </AuthContext.Provider>
   );
--- app/register/index.tsx
+++ app/register/index.tsx
@@ -1,3 +1,6 @@
+import { useContext, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { AuthContext } from "../_contexts/auth";
 import { Platform } from "react-native";
 
 import {
@@ -11,25 +14,64 @@ import {
 } from "../(login)/styles";
 
 const RegisterPage = () => {
+  const { signUp, loading } = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUpPress = async () => {
+    const signUpUser = {
+      name,
+      email,
+      password,
+    };
+
+    await signUp({ signUpUser });
+
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <Background>
       <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
         <Logo source={require("../../assets/images/Logo.png")} alt="Logo" />
 
         <AreaInput>
-          <Input placeholder="Seu nome" />
+          <Input
+            placeholder="Seu nome"
+            value={name}
+            onChangeText={(name) => setName(name)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Seu e-mail" />
+          <Input
+            placeholder="Seu e-mail"
+            value={email}
+            onChangeText={(email) => setEmail(email)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Sua senha" />
+          <Input
+            placeholder="Sua senha"
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+            secureTextEntry={true}
+          />
         </AreaInput>
 
-        <SubmitButton activeOpacity={0.8}>
-          <SubmitText>Cadastrar</SubmitText>
+        <SubmitButton activeOpacity={0.8} onPress={handleSignUpPress}>
+          <SubmitText>
+            {loading ? (
+              <ActivityIndicator size={20} color="#fff" />
+            ) : (
+              "Cadastrar"
+            )}
+          </SubmitText>
         </SubmitButton>
       </Container>
     </Background>
