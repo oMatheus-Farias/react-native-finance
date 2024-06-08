17ad350690e9afcb415a1ffe715bd9b2c54ce524
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-06-08 14:29:15 -0300

    feat: add register page

--- app/register/index.tsx
+++ app/register/index.tsx
@@ -1,16 +1,38 @@
-import { View, Text } from "react-native";
+import { Platform } from "react-native";
+
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+} from "../(login)/styles";
 
 const RegisterPage = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>register page</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <Logo source={require("../../assets/images/Logo.png")} alt="Logo" />
+
+        <AreaInput>
+          <Input placeholder="Seu nome" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Seu e-mail" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 };
 
